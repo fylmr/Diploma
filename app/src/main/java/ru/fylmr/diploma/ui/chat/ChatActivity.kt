d9e8eb821db57f599d1a16918d614e514666077f
Philipp Maksimov <filipp.39@gmail.com>
2020-08-25 21:43:30 +0200

    Добавил запрос разрешений

--- app/src/main/java/ru/fylmr/diploma/ui/chat/ChatActivity.kt
+++ app/src/main/java/ru/fylmr/diploma/ui/chat/ChatActivity.kt
@@ -1,6 +1,10 @@
 package ru.fylmr.diploma.ui.chat
 
+import android.Manifest.permission.*
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.ac_main.*
@@ -8,6 +12,7 @@ import moxy.MvpAppCompatActivity
 import moxy.presenter.InjectPresenter
 import ru.fylmr.diploma.R
 
+@ExperimentalStdlibApi
 class ChatActivity : MvpAppCompatActivity(), ChatView {
 
     private val adapter by lazy { ChatAdapter() }
@@ -19,6 +24,8 @@ class ChatActivity : MvpAppCompatActivity(), ChatView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ac_main)
 
+        runPermissionChecks()
+
         recycler.adapter = adapter
         val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         manager.stackFromEnd = true
@@ -29,11 +36,45 @@ class ChatActivity : MvpAppCompatActivity(), ChatView {
         }
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != REQUEST_PERMISSIONS_CODE) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults.any { it != PERMISSION_GRANTED }) {
+            Toast.makeText(this, "Разрешений недостаточно!", Toast.LENGTH_LONG).show()
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    // ===================================================
+    //
+    // ===================================================
+
     override fun addMessages(messages: List<Message>, clearField: Boolean) {
         if (clearField) {
             messageEditText.setText("")
         }
 
         adapter.setData(messages)
+    }
+
+    // ===================================================
+    //
+    // ===================================================
+
+    private fun runPermissionChecks() {
+        val permissions = arrayOf(BLUETOOTH, BLUETOOTH_ADMIN, ACCESS_FINE_LOCATION)
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSIONS_CODE)
+    }
+
+    companion object {
+        private const val REQUEST_PERMISSIONS_CODE = 1000
     }
 }
